e1b816bb000cfbebf067e83331b562119bc16aac
Matthew Johnson <mattjj@csail.mit.edu>
2016-10-25 13:41:34 -0700

    sketch out basic implementation of cython code

--- autograd_linalg/cython_linalg.pyx
+++ autograd_linalg/cython_linalg.pyx
@@ -4,9 +4,24 @@
 import numpy as np
 cimport numpy as np
 cimport cython
-from scipy.linalg.cython_blas cimport dsymv, ddot, dgemm
-from scipy.linalg.cython_lapack cimport dtrtrs, dpotrf, dpotrs, dpotri
+from scipy.linalg.cython_lapack cimport dtrtrs
 
-def solve_triangular(double[::1,:,:] L, double[::1,:,:] X, trans=False, lower=False):
-    '''Just like scipy.linalg.solve_triangular, except broadcasts over leading dimensions'''
-    pass
+def solve_triangular(double[::1,:,:] L, double[::1,:,:] X, int trans, int lower):
+    cdef:
+        int i, K = L.shape[2]
+        double[::1,:,:] out = np.empty_like(X)
+        char _trans = 'T' if trans else 'N', _lower = 'L' if lower else 'U'
+
+    for i in range(K):
+        _solve_triangular(L[:,:,i], X[:,:,i], out[:,:,i], _trans, _lower)
+
+cdef inline void _solve_triangular(
+        double[::1,:] L, double[::1,:] X, double[::1,:] out,
+        char trans, char lower):
+    cdef:
+        int M = X.shape[0], N = X.shape[1], info = 0, i, j
+
+    for j in range(N):
+        for i in range(M):
+            out[i,j] = X[i,j]
+    dtrtrs(&lower, &trans, 'N', &M, &N, &L[0,0], &M, &out[0,0], &M, &info)
--- autograd_linalg/linalg.py
+++ autograd_linalg/linalg.py
@@ -4,13 +4,17 @@ from autograd.core import primitive
 from autograd.scipy.linalg import _flip
 
 import cython_linalg as cyla
-from util import T, symm, al2d
+from util import T, symm, al2d, F
 
 @primitive
 def solve_triangular(a, b, trans=0, lower=False, **kwargs):
-    flat_a = np.require(np.reshape(a, (-1,) + a.shape[-2:]), order='F')
-    flat_b = np.require(np.reshape(b, flat_a.shape[:-1] + (1,)), order='F')
-    flat_result = cyla.solve_triangular(flat_a, flat_b, trans=trans, lower=lower, **kwargs)
+    '''Just like scipy.linalg.solve_triangular on real arrays, except this
+    function broadcasts over leading dimensions like np.linalg.solve.'''
+    F = lambda X: np.require(np.transpose(X, (1, 2, 0)), np.double, 'F')
+    flat_a = np.reshape(a, (-1,) + a.shape[-2:])
+    flat_b = np.reshape(b, flat_a.shape[:-1] + (1,))
+    flat_result = cyla.solve_triangular(F(flat_a), F(flat_b),
+                                        trans=trans, lower=lower)
     return np.reshape(flat_result, b.shape)
 
 def make_grad_solve_triangular(ans, a, b, trans=0, lower=False, **kwargs):
