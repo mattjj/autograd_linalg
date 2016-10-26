8f426dd199fa8cf156589f8cf1b24e08c006bb1b
Matthew Johnson <mattjj@csail.mit.edu>
2016-10-25 22:45:30 -0700

    fix cholesky bugs

--- autograd_linalg/linalg.py
+++ autograd_linalg/linalg.py
@@ -4,7 +4,7 @@ from autograd.core import primitive
 from autograd.scipy.linalg import _flip
 
 import cython_linalg as cyla
-from util import T, symm
+from util import T, symm, C
 
 ### solve_triangular
 
@@ -14,18 +14,18 @@ def solve_triangular(a, b, trans=0, lower=False, **kwargs):
     function broadcasts over leading dimensions like np.linalg.solve.'''
     flat_a = np.reshape(a, (-1,) + a.shape[-2:])
     flat_b = np.reshape(b, flat_a.shape[:-1] + (-1,))
-    flat_result = cyla.solve_triangular(flat_a, flat_b,
+    flat_result = cyla.solve_triangular(C(flat_a), C(flat_b),
                                         trans=trans, lower=lower)
     return np.reshape(flat_result, b.shape)
 
 def make_grad_solve_triangular(ans, a, b, trans=0, lower=False, **kwargs):
     tri = anp.tril if (lower ^ (_flip(a, trans) == 'N')) else anp.triu
     transpose = lambda x: x if _flip(a, trans) != 'N' else T(x)
-    ans = np.reshape(ans, a.shape[:-1] + (-1,))
+    ans = anp.reshape(ans, a.shape[:-1] + (-1,))
 
     def solve_triangular_grad(g):
         v = solve_triangular(a, g, trans=_flip(a, trans), lower=lower)
-        return -transpose(tri(anp.matmul(np.reshape(v, ans.shape), T(ans))))
+        return -transpose(tri(anp.matmul(anp.reshape(v, ans.shape), T(ans))))
 
     return solve_triangular_grad
 solve_triangular.defgrad(make_grad_solve_triangular)
@@ -37,7 +37,7 @@ solve_triangular.defgrad(lambda ans, a, b, trans=0, lower=False, **kwargs:
 
 solve_trans = lambda L, X: solve_triangular(L, X, lower=True, trans='T')
 solve_conj = lambda L, X: solve_trans(L, T(solve_trans(L, T(X))))
-phi = lambda X: anp.tril(X) / 1. + anp.eye(X.shape[-1])
+phi = lambda X: anp.tril(X) / (1. + anp.eye(X.shape[-1]))
 
 cholesky = primitive(np.linalg.cholesky)
-cholesky.defgrad(lambda L, A: lambda g: symm(solve_conj(L, phi(anp.matmul(L, g)))))
+cholesky.defgrad(lambda L, A: lambda g: symm(solve_conj(L, phi(anp.matmul(T(L), g)))))
--- autograd_linalg/util.py
+++ autograd_linalg/util.py
@@ -2,3 +2,4 @@ import autograd.numpy as np
 
 T = lambda x: np.swapaxes(x, -1, -2)
 symm = lambda x: (x + T(x)) / 2.
+C = lambda x: np.require(x, x.dtype, 'C')
