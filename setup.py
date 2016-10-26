0999a9ec291816a565dbb26b3ef0074672d652c3
Matthew Johnson <mattjj@csail.mit.edu>
2016-10-25 17:35:16 -0700

    list requirements in setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,12 @@ import numpy as np
 from Cython.Build import cythonize
 
 setup(
+    name='autograd_linalg',
+    author='Matthew James Johnson',
+    author_email='mattjj@csail.mit.edu',
+    packages=['autograd_linalg'],
     ext_modules=cythonize('**/*.pyx'),
     include_dirs=[np.get_include(),],
+    install_requires=['numpy >= 1.10.0', 'cython >= 0.24', 'scipy >= 0.18.1',
+                      'autograd'],
 )
